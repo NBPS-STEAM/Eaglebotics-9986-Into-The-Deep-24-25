e4e2bba27508985acd8b6e14dff52b3d505a8f62
Kelin Levine <kelin_levine@mynbps.org>
2024-10-26 12:38:23 -0400

    Add TestingTeleOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TestingSubsystem.java
@@ -0,0 +1,332 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import android.util.Pair;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+
+/**
+ * This is a class for a "subsystem" which tests motors and servos.
+ * It detects all hardware devices and allows the driver to choose between any
+ * motor, servo, or continuous rotation servo to manipulate.
+ *
+ * <p>For more information, see TestingTeleOpMode.</p>
+ */
+public class TestingSubsystem extends SubsystemBase {
+    enum HardwareOption {
+        NONE,
+        DC_MOTOR,
+        SERVO,
+        CR_SERVO,
+    }
+
+
+    // Private Instance Variables
+    private final HardwareMap hardwareMap;
+    private final Telemetry telemetry;
+
+    private List<Pair<String, HardwareOption>> allDevices;
+    private int motorCount;
+    private int servoCount;
+    private int crServoCount;
+
+    private int selectedDeviceIndex;
+
+    private Integer currentDeviceIndex;
+    private HardwareOption currentHardware;
+    private Object currentDevice;
+    private Double frozenMoveAmount;
+
+
+    // Constructor methods
+    public TestingSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+
+        reloadDeviceList();
+    }
+
+
+    // Methods
+
+    // Device selection
+
+    /**
+     * Reload the list of motors, servos, and continuous rotation servos configured on the robot.
+     * This checks using the list of configured devices. Hardware changes (i.e. plugging/unplugging)
+     * will not be reflected in the new list unless the configuration is changed too.
+     * <p>This will deactivate the currently activated device, if there is one.</p>
+     * <p>This is run on initialization.</p>
+     */
+    public void reloadDeviceList() {
+        deactivateCurrent();
+
+        selectedDeviceIndex = 0;
+
+        allDevices = new ArrayList<>();
+        motorCount = addAllDeviceNames(DcMotor.class, HardwareOption.DC_MOTOR);
+        servoCount = addAllDeviceNames(Servo.class, HardwareOption.SERVO);
+        crServoCount = addAllDeviceNames(CRServo.class, HardwareOption.CR_SERVO);
+    }
+
+    /**
+     * Determine whether any motors, servos, or continuous rotation servos are configured on the robot.
+     * This is true as of initialization or the last time {@link #reloadDeviceList()} was run.
+     */
+    public boolean hasNoDevices() {
+        return allDevices.isEmpty();
+    }
+
+    /**
+     * @return Whether there is an active device
+     */
+    public boolean isActiveDevice() {
+        return currentDevice != null;
+    }
+
+    /**
+     * Increment the index of the selected device by {@code inc} places.
+     * If the new index would be out of bounds, it will wrap around.
+     */
+    public void changeSelection(int inc) {
+        // Sanity check: make sure there are devices
+        if (hasNoDevices()) return;
+        // % is the modulus operator.
+        // If selectedDevice + inc is out of bounds of allDevices, it will wrap around.
+        selectedDeviceIndex = (selectedDeviceIndex + inc) % allDevices.size();
+    }
+
+    /**
+     * Activate the currently selected device. This will initialize the hardware.
+     */
+    public void activateSelected() {
+        // Sanity check: make sure there are devices
+        if (hasNoDevices()) return;
+
+        deactivateCurrent();
+
+        currentDeviceIndex = selectedDeviceIndex;
+        currentHardware = allDevices.get(selectedDeviceIndex).second;
+        currentDevice = null;
+        frozenMoveAmount = null;
+
+        switch (currentHardware) {
+            case DC_MOTOR:
+                DcMotor motor = hardwareMap.get(DcMotor.class, allDevices.get(selectedDeviceIndex).first);
+                motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motor.setDirection(DcMotor.Direction.FORWARD);
+                motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                currentDevice = motor;
+                break;
+
+            case SERVO:
+                Servo servo = hardwareMap.get(Servo.class, allDevices.get(selectedDeviceIndex).first);
+                servo.setDirection(Servo.Direction.FORWARD);
+                currentDevice = servo;
+                break;
+
+            case CR_SERVO:
+                CRServo crServo = hardwareMap.get(CRServo.class, allDevices.get(selectedDeviceIndex).first);
+                crServo.setDirection(CRServo.Direction.FORWARD);
+                currentDevice = crServo;
+                break;
+        }
+    }
+
+    /**
+     * Deactivate the currently activated device. This will close the hardware.
+     */
+    public void deactivateCurrent() {
+        if (currentDevice != null) { // sanity check; probably not necessary
+            switch (currentHardware) {
+                case DC_MOTOR:
+                    ((DcMotor) currentDevice).close();
+                    break;
+                case SERVO:
+                    ((Servo) currentDevice).close();
+                    break;
+                case CR_SERVO:
+                    ((CRServo) currentDevice).close();
+                    break;
+            }
+        }
+        currentDeviceIndex = null;
+        currentHardware = HardwareOption.NONE;
+        currentDevice = null;
+        frozenMoveAmount = null;
+    }
+
+    // Device manipulation
+
+    /**
+     * Move the currently activated motor, servo, or cr-servo.
+     * For a motor or continuous rotation servo, {@code amount} is the amount of power.
+     * For a servo, {@code amount} is the target position.
+     */
+    public void moveCurrent(double amount) {
+        if (isFrozen()) amount = frozenMoveAmount;
+        switch (currentHardware) {
+            case DC_MOTOR:
+                ((DcMotor) currentDevice).setPower(amount);
+                break;
+            case SERVO:
+                ((Servo) currentDevice).setPosition(amount);
+                break;
+            case CR_SERVO:
+                ((CRServo) currentDevice).setPower(amount);
+                break;
+        }
+    }
+
+    /**
+     * Zero the currently activated device (if it can be zeroed).
+     */
+    public void zeroCurrent() {
+        switch (currentHardware) {
+            case DC_MOTOR:
+                ((DcMotor) currentDevice).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                ((DcMotor) currentDevice).setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                break;
+        }
+    }
+
+    /**
+     * Freeze the currently activated device at the amount of power/position given here.
+     * Every time moveCurrent() is called while the device is frozen, the frozen value
+     * will be used in place of {@code amount}.
+     */
+    public void freezeMovement(double amount) {
+        frozenMoveAmount = amount;
+    }
+
+    /**
+     * Unfreeze the currently activated device, so that calls to moveCurrent() obey the {@code amount} given there.
+     * @see #freezeMovement(double)
+     */
+    public void unfreezeMovement() {
+        frozenMoveAmount = null;
+    }
+
+    /**
+     * Whether the currently activated device is frozen.
+     * @see #freezeMovement(double)
+     */
+    public boolean isFrozen() {
+        return frozenMoveAmount != null;
+    }
+
+    /**
+     * Freeze the currently activated device with this amount if not frozen, otherwise unfreeze.
+     * @see #freezeMovement(double)
+     */
+    public void toggleFrozen(double amount) {
+        if (isFrozen()) unfreezeMovement();
+        else freezeMovement(amount);
+    }
+
+    // Telemetry
+
+    public void reportTelemetry() {
+        // Clear old data
+        telemetry.clear();
+
+        // List some data
+        telemetry.addLine("!! ROBOT TESTING MODE !!");
+        telemetry.addLine();
+        telemetry.addData("Motors found", motorCount);
+        telemetry.addData("Servos found", servoCount);
+        telemetry.addData("Continuous rotation servos found", crServoCount);
+        if (hasNoDevices()) {
+            telemetry.addLine();
+            telemetry.addLine("WARNING: There are no motors, servos, or continuous rotation servos configured!");
+        }
+        telemetry.addLine();
+        telemetry.addLine();
+
+        // Give instructions
+        telemetry.addLine("If a device is missing from the list, make sure it is configured using 'Configure Robot' on the Driver Station.");
+        telemetry.addLine();
+        telemetry.addLine("Select a device with the d-pad, then press A to activate.");
+        telemetry.addLine("Deactivate the currently active device by pressing B.");
+        telemetry.addLine("Move the active device with the left stick up/down.");
+        telemetry.addLine("Freeze the position of the left stick by pressing X. Press again to unfreeze.");
+        telemetry.addLine("Zero the active device by pressing Y. Servos can't be zeroed.");
+        telemetry.addLine("Reload the list of devices by pressing Start. This checks the configuration in 'Configure Robot', not physically attached hardware.");
+        telemetry.addLine();
+        telemetry.addLine();
+
+        // List all devices (indicate whether selected or activated)
+        telemetry.addLine("All configured motors:");
+        reportDeviceNames(0, motorCount);
+        telemetry.addLine();
+        telemetry.addLine("All configured servos:");
+        reportDeviceNames(motorCount, motorCount + servoCount);
+        telemetry.addLine();
+        telemetry.addLine("All configured continuous rotation servos:");
+        reportDeviceNames(motorCount + servoCount, motorCount + servoCount + crServoCount);
+        telemetry.addLine();
+        telemetry.addLine();
+
+        // Display info of currently active device
+        if (isActiveDevice()) {
+            reportActiveDevice();
+        }
+
+        // Send data to the Driver Station
+        telemetry.update();
+    }
+
+
+    // Helper Methods
+
+    /**
+     * Adds the names of all configured devices in the hardware map matching
+     * the given type to allDevices with the given HardwareOption type.
+     * @return The number of names that were added
+     */
+    private int addAllDeviceNames(Class<? extends HardwareDevice> clazz, HardwareOption type) {
+        SortedSet<String> allNames = hardwareMap.getAllNames(clazz);
+        for (String device : allNames) {
+            allDevices.add(new Pair<>(device, type));
+        }
+        return allNames.size();
+    }
+
+    /**
+     * Add, to telemetry, the devices named in allDevices within the range {@code start} to {@code end}
+     * with a symbol for each one indicating whether it is selected or active.
+     */
+    private void reportDeviceNames(int start, int end) {
+        for (int i = start; i < end; i++) {
+            String status = (i == selectedDeviceIndex ? ">" : " ") + (i == currentDeviceIndex ? "@" : " ");
+            telemetry.addData(status, allDevices.get(i).first);
+        }
+    }
+
+    private void reportActiveDevice() {
+        telemetry.addData("Currently active device", allDevices.get(currentDeviceIndex).first + " (" + currentDevice.getClass().getCanonicalName() + ")");
+        switch (currentHardware) {
+            case DC_MOTOR:
+                telemetry.addData("Motor power", ((DcMotor) currentDevice).getPower());
+                telemetry.addData("Motor position", ((DcMotor) currentDevice).getCurrentPosition());
+                break;
+            case SERVO:
+                telemetry.addData("Servo position", ((Servo) currentDevice).getPosition());
+                break;
+            case CR_SERVO:
+                telemetry.addData("Continuous rotation servo power", ((CRServo) currentDevice).getPower());
+                break;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestingTeleOpMode.java
@@ -0,0 +1,126 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.TestingSubsystem;
+
+/*
+ * This file contains a simple example "OpMode" for driving a robot.
+ * An OpMode (Operation Mode) is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The OpMode to use can be selected on the FTC Driver Station.
+ *
+ * This code allows for manipulating individual motors and servos to test positions, power, direction, etc.
+ * When the opmode starts, follow the instructions on the Driver Station to select and control a device.
+ *
+ * Hardware devices (motors/servos) are not initialized until they are selected and activated, and
+ * will be closed when deactivated. When a motor is initialized, it will have its encoder value reset.
+ *
+ *
+ * The initialize() method runs when the "init" button is pressed on the Driver Station.
+ * Everything afterward is handled by commands.
+ *
+ *
+ *
+ * Robot controls:
+ * Controller 1:
+ * D-pad up/down        |   Move up or down the list to select a hardware device (motor/servo)
+ * South (A/X) button   |   Activate the selected hardware device (deactivating the previously active one, if any)
+ * East (B/○) button    |   Deactivate the currently active hardware device
+ * West (X/□) button    |   Toggle freezing the position of the left stick
+ * North (Y/Δ) button   |   Zero the active hardware device
+ * Left stick up        |   Move the active hardware device forward (+)
+ * Left stick down      |   Move the active hardware device backward (-)
+ * Start button         |   Reload the list of CONFIGURED devices (physically plugging/unplugging a device will not result in a change)
+ *
+ *
+ * Tips:
+ * If you don't understand what a particular method does, then hover your mouse over it to get some info and a short description.
+ * If you click on a variable/method, then all uses of it in the file will be highlighted.
+ * Click on a variable/method and press Ctrl+B (Cmd+B on mac) to be taken to where it was originally declared.
+ * In addition, do that twice for a list of everywhere that variable/method is used.
+ * You can also do that by holding ctrl while you click on the variable/method.
+ */
+
+@TeleOp(name="Testing TeleOp Mode", group="Driver OpMode")
+public class TestingTeleOpMode extends CommandOpMode {
+
+    // Hardware Variables
+    private GamepadEx gamepad;
+    private TestingSubsystem testingSubsystem;
+
+
+    // This is run when the "INIT" button is pressed on the Driver Station.
+    @Override
+    public void initialize() {
+        // Initialize hardware variables
+        gamepad = new GamepadEx(gamepad1);
+        testingSubsystem = new TestingSubsystem(hardwareMap, telemetry);
+
+
+        // Controller bindings
+
+        // Device Selection
+
+        bindTo(Button.DPAD_UP, () -> testingSubsystem.changeSelection(1)); // Move up the list to select a hardware device (motor/servo)
+        bindTo(Button.DPAD_DOWN, () -> testingSubsystem.changeSelection(-1)); // Move down the list to select a hardware device (motor/servo)
+        bindTo(Button.A, testingSubsystem::activateSelected); // Activate the selected hardware device (deactivating the previously active one, if any)
+        bindTo(Button.B, testingSubsystem::deactivateCurrent); // Deactivate the currently active hardware device
+
+        // Device Control
+
+        bindTo(Button.X, () -> testingSubsystem.toggleFrozen(gamepad.getLeftY())); // Toggle freezing the position of the left stick
+        bindTo(Button.Y, testingSubsystem::zeroCurrent); // Zero the active hardware device
+        bindTo(Button.START, testingSubsystem::reloadDeviceList); // Reload the list of CONFIGURED devices (physically plugging/unplugging a device will not result in a change)
+
+        schedule(new RunCommand(() -> testingSubsystem.moveCurrent(gamepad.getLeftY()))); // Move the active hardware device
+
+
+        // Report Telemetry (Default Command)
+
+        testingSubsystem.setDefaultCommand(new RunCommand(testingSubsystem::reportTelemetry, testingSubsystem));
+    }
+
+
+    // Helper Methods
+
+    /**
+     * Bind an action to run through an instant command when a button is pressed on the gamepad.
+     */
+    public void bindTo(Button button, Runnable action) {
+        gamepad.getGamepadButton(button).whenPressed(new InstantCommand(action));
+    }
+}
